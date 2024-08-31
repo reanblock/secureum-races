2819dd2ce1de01b146e4e903990a649dd04e5487
Darren Jensen <darren.jensen@gmail.com>
2024-08-31 16:35:06 +0700

    added R1_Q4

--- test/RACE1.t.sol
+++ test/RACE1.t.sol
@@ -5,12 +5,14 @@ pragma abicoder v2;
 import { Test, console } from "forge-std/Test.sol";
 import { InSecureumToken } from "../src/Race1.sol";
 
-contract R1_Q1 is Test {
+contract InSecureumTokenTestBase is Test {
     InSecureumToken token;
     function setUp() public {
         token = new InSecureumToken();
     }
+}
 
+contract R1_Q1 is InSecureumTokenTestBase {
     function test_lockedEther() public {
         // buy 10 tokens
         token.buy{value: 1 ether}(10);
@@ -35,5 +37,23 @@ contract R1_Q1 is Test {
         Since there are no more public functions available the 
         Ether is therefore locked in the contract!
         */ 
+    }
+}
+
+// NOTE Q2, Q2 and Q3 do not require tests, moving to Q4 below:
+
+contract R1_Q4 is InSecureumTokenTestBase {
+    function test_UnsafeRoundingAllowsUsersToReceiveTokensForFree() public {
+        /*
+            In the `InSecureumToken` contract `buy` function it divides `desired_tokens` first 
+            and only then multiplies by the decimals, this causes any amount of tokens below 10 
+            to result in 0 required_wei_sent.
+        */
+        // request to buy 9 tokens (without sending any Ether value)
+        token.buy(9);
+        // tokens received (without paying for them)
+        assertEq(token.balances(address(this)), 9);
+        // confirmed the token contract has not received any ether. Token ccontract balance is still 0 ether.
+        assertEq(address(token).balance, 0);
     }
 }
